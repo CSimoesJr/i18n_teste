c49c1420729ae3207c18f453f9b29a96bfe48dc9
Debora Frias Constantino <debora.constantino@totvs.com.br>
2025-03-05 13:39:02 -0300

    Teste i18n do po-ui v19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PoI18nService } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'teste-i18n';
+  literals: any = '';
+
+  constructor(private poI18nService: PoI18nService) {
+    poI18nService.getLiterals()
+      .subscribe((literals) => {
+        this.literals = literals;
+        console.log('APP', literals);
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,28 @@
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PoI18nModule, PoModule } from '@po-ui/ng-components';
+import { PoTemplatesModule } from '@po-ui/ng-templates';
+import { ProtheusLibTHFModule } from '@framesp/protheus-lib-thf';
+
+const i18nConfig: any = {
+  default: {
+    language: 'pt-BR',
+    context: 'general',
+    cache: true
+  },
+  contexts: {
+    general: {
+      'pt-BR': {
+        teste: 'Texto do app',
+        str0004: 'Texto do app'
+      }
+    }
+  }
+};
 
 @NgModule({
   declarations: [
@@ -10,9 +30,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PoModule,
+    PoTemplatesModule,
+    ProtheusLibTHFModule,
+    PoI18nModule.config(i18nConfig)
   ],
-  providers: [],
+  providers: [
+    provideHttpClient(withInterceptorsFromDi()),
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
